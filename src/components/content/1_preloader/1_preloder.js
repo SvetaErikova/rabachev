d191a381f66f914610c61cd33cfd8dd0ceac7f3c
user <svetaerikova310@gmail.com>
2025-03-09 20:39:17 +0300

    preloader

--- src/components/content/1_preloader/1_preloder.js
+++ src/components/content/1_preloader/1_preloder.js
@@ -1,24 +1,30 @@
-// let preloader = document.querySelector('.preloader'),
-// preloader__logo = preloader.querySelector('.preloader__logo')
-// gsap.to(preloader__logo, {
-//   scale: 1.3,
-//   opacity: 1,
-//   filter: "blur(0px)",
-//   duration: 2,
-//   onComplete: () => {
-//
-//     function preloaderAfterLoad(){
-//       gsap.to(preloader, {  transform: 'scaleY(0)',}, );
-//     }
-//     function checkPageLoad() {
-//       if (document.readyState === 'complete' || document.readyState === 'interactive') {
-//         preloaderAfterLoad()
-//         clearInterval(intervalId);
-//       }
-//     }
-//     const intervalId = setInterval(checkPageLoad, 500);
-//   }
-// });
-// setTimeout(() => {
-//   window.scroll({ top: -1, left: 0, behavior: "smooth" });
-// }, 300);
+
+
+
+function preloaderStart(){
+  let preloader = document.querySelector('.preloader'),
+preloader__logo = preloader.querySelector('.preloader__logo')
+gsap.to(preloader__logo, {
+  scale: 1.3,
+  opacity: 1,
+  filter: "blur(0px)",
+  duration: 2,
+  onComplete: () => {
+
+    function preloaderAfterLoad(){
+      gsap.to(preloader, {  transform: 'scaleY(0)',}, );
+    }
+    function checkPageLoad() {
+      if (document.readyState === 'complete' || document.readyState === 'interactive') {
+        preloaderAfterLoad()
+        clearInterval(intervalId);
+      }
+    }
+    const intervalId = setInterval(checkPageLoad, 500);
+  }
+});
+setTimeout(() => {
+  window.scroll({ top: -1, left: 0, behavior: "smooth" });
+}, 300);
+}
+preloaderStart()
