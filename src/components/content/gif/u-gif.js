ee9c218c5a4546d1df7e267b9f0467bb3b59108d
SvetaErikova <svetaerikova310@gmail.com>
2025-03-26 16:19:52 +0300

    fix

--- src/components/content/gif/u-gif.js
+++ src/components/content/gif/u-gif.js
@@ -17,18 +17,31 @@ function animationGifDesk() {
 function animationGifMob() {
   const hero = document.querySelector(".u-hero__container");
 
-  // Создаем анимацию с помощью GSAP и ScrollTrigger
   gsap.to(hero, {
     scrollTrigger: {
       trigger: hero,
       start: "top top",
       end: "bottom end: `+=${window.innerHeight}`,",
-      scrub: true, // Анимация будет плавно изменяться при прокрутке
-      pin: true, // Фиксируем .u-hero на месте
-      pinSpacing: false, // Не добавляем отступы вокруг фиксированного блока
+      scrub: true,
+      pin: true,
+      pinSpacing: false,
       // markers: true,
     },
   });
+  gsap.fromTo(
+    ".u-gif__title",
+    { bottom: "85%" },
+    {
+      bottom: "0%",
+      scrollTrigger: {
+        trigger: ".u-gif",
+        start: "top 80%",
+        end: `bottom top`,
+        scrub: true,
+        markers: true,
+      },
+    },
+  );
 }
 
 if (window.matchMedia("(min-width: 641px)").matches) {
